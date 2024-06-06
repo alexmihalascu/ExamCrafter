063c629bd8efb5d5e78ede3cd6c932c5d7f35254
Alex Mihalașcu <alexmihalascu@gmail.com>
2024-06-06 23:39:30 +0300

    vercel 2.0

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {
-      VITE_SUPABASE_URL: JSON.stringify(process.env.VITE_SUPABASE_URL),
-      VITE_SUPABASE_KEY: JSON.stringify(process.env.VITE_SUPABASE_KEY),
-      VITE_CLERK_PUBLISHABLE_KEY: JSON.stringify(process.env.VITE_CLERK_PUBLISHABLE_KEY),
-    }
+    'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'process.env.VITE_SUPABASE_KEY': JSON.stringify(process.env.VITE_SUPABASE_KEY),
+    'process.env.VITE_CLERK_PUBLISHABLE_KEY': JSON.stringify(process.env.VITE_CLERK_PUBLISHABLE_KEY)
+  },
+  build: {
+    outDir: 'dist',
   }
 });
